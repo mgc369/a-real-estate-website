20274a23b2421402518183520b1503b444f623d6
john <blackcar@dcc.com>
2022-06-27 17:54:57 +0600

    somechanges2

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 let dateq = new Date(2021, 1, 1);
 let finaldate = dateq.getFullYear();
-setTimeout(function () {
+setInterval(() => {
     document.getElementById("date").innerHTML = finaldate;
 }, 3.154e+10);
